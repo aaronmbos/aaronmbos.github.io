4b24d7949e1d89e733db768ec66de47033dca313
Aaron Bos <aaronmbos@gmail.com>
2022-10-05 05:58:35 -0500

    Add Apollo GraphQL client and implement example query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error 🙁</p>;
+
+  return data.locations.map((location: any) => (
+    <div key={location.id}>
+      <h3>{location.name}</h3>
+      <img
+        width="400"
+        height="250"
+        alt="location-reference"
+        src={`${location.photo}`}
+      />
+      <br />
+      <b> About this location:</b>
+      <p>{location.description}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>My first Apollo app 🚀</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +52,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DisplayLocations />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://flyby-gateway.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
