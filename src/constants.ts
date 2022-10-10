43b8a81ab78128ca851769e3c07ed446f4e6fece
Aaron Bos <aaronmbos@gmail.com>
2022-10-10 06:21:13 -0500

    Update token

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 // This token is read-only, I'm ok having it in code
-export const AUTH_TOKEN: string = "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";
+export const AUTH_TOKEN: string = "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";
 export const GITHUB_GRAPHQL_ENDPOINT: string = "https://api.github.com/graphql";
